ee35e6fcb99e3b96e4c3fcdc00a0b4508a72cf1e
Philip Gillißen <guerda@freenet.de>
2022-09-20 20:03:19 +0200

    :recycle: Extract function

--- osmdownloader.py
+++ osmdownloader.py
@@ -1,6 +1,7 @@
 from utils import WattFormatter, PowerParser
 import overpass
 import logging
+from generator import SolarGenerator
 
 
 class OsmDownloader:
@@ -20,8 +21,16 @@ class OsmDownloader:
         )
         return r
 
-    def parse_power(power_text):
-        return PowerParser.parse(power_text)
+    def create_generator(self, feature):
+        generator = SolarGenerator()
+        generator.is_commercial = "ref" in f.properties and "operator" in f.properties
+
+        generator.capacity = 0
+        if "generator:output:electricity" in f.properties:
+            generator.capacity = PowerParser.parse(
+                f.properties["generator:output:electricity"]
+            )
+        self.log.debug(f.properties)
 
 
 if __name__ == "__main__":
@@ -34,19 +43,14 @@ if __name__ == "__main__":
     od = OsmDownloader()
     r = od.get_solar_generators(40667)
     logging.debug(r)
-    sum_power = 0
-    count_commercials = 0
-    # TODO extract to function
+    generators = []
     for f in r.features:
-        is_commercial = "ref" in f.properties and "operator" in f.properties
-        if is_commercial:
-            count_commercials += 1
-        if "generator:output:electricity" in f.properties:
-            power = OsmDownloader.parse_power(
-                f.properties["generator:output:electricity"]
-            )
-            sum_power += power
-        logging.info(f.properties)
-    logging.info("%d PV generators" % len(r.features))
+        generator = od.create_generator(f)
+        generators.append(generator)
+        # sum_power += power
+
+    logging.info("%d PV generators" % len(generators))
+    sum_power = sum(g.capacity for g in generators)
+    count_commercials = len(g for g in generators if g.is_commercial)
     logging.info("Total sum of at least %d watt peak" % WattFormatter.format(sum_power))
     logging.info("%d commercial generators" % count_commercials)
