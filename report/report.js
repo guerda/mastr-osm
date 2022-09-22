eeea832ad181b3430b92f56140d99d8d4227d39a
Philip Gillißen <guerda@freenet.de>
2022-09-22 08:11:02 +0200

    Add bootstrap to report, draw both lines

--- report/report.js
+++ report/report.js
@@ -1,15 +1,24 @@
-fetch('data/40667.json')
-    .then((response) => response.json())
-    .then((progressData) => {         
-        progressPlotElement = document.getElementById("progress-plot")
-        Plotly.newPlot(progressPlotElement, [
-                {
-                    x: progressData["dates"], 
-                    y: progressData["solarGenerators"]
-                }
-            ], { 
-                margin: {
-                    t: 0 
-                }
-            });
-});
+fetch("data/40667.json")
+  .then((response) => response.json())
+  .then((progressData) => {
+    progressPlotElement = document.getElementById("progress-plot");
+    layout = {
+      title: "Registered and mapped solar generators by time",
+    };
+    plot = Plotly.newPlot(
+      progressPlotElement,
+      [
+        {
+          x: progressData["dates"],
+          y: progressData["solarGenerators"],
+          name: "Registered in MaStR",
+        },
+        {
+          x: progressData["dates"],
+          y: progressData["solarGeneratorsMapped"],
+          name: "Mapped in OSM",
+        },
+      ],
+      layout
+    );
+  });
