b8161f8f0ae9762425308ce7f46182402bffd72f
Philip Gillißen <guerda@freenet.de>
2023-07-20 10:29:00 +0200

    Fix municipality test

--- tests/municipality_test.py
+++ tests/municipality_test.py
@@ -21,9 +21,9 @@ def test_represent_as_json():
     m.solarGeneratorsMapped = [0, 0, 1]
     m_json = m.json()
     assert (
-        """{"dates":["2022-01-31T14:01:03.000003"], "solarGenerators":[0,1,2],"""
-        """ "solarGeneratorsMapped":[0,0,1],"""
-        """ "missingCommercialGenerators":null}""" == m_json
+        """{"dates":["2022-01-31T14:01:03.000003"],"solarGenerators":[0,1,2],"""
+        """"solarGeneratorsMapped":[0,0,1],"""
+        """"missingCommercialGenerators":null}""" == m_json
     )
 
 
